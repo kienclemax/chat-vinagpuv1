3271d40e89fbf59aed5db3f7cb7280d3695142a7
clemax <contact@clemax.net>
2025-08-27 14:10:11 +0700

    f

--- backend/src/users/dto/update-user.dto.ts
+++ backend/src/users/dto/update-user.dto.ts
@@ -1,8 +1,31 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateUserDto } from './create-user.dto';
-import { IsOptional } from 'class-validator';
+import { IsOptional, IsEmail, IsString, MinLength } from 'class-validator';
 
 export class UpdateUserDto extends PartialType(CreateUserDto) {
+  @IsEmail()
   @IsOptional()
+  email?: string;
+
+  @IsString()
+  @IsOptional()
+  @MinLength(3)
+  username?: string;
+
+  @IsString()
+  @IsOptional()
+  @MinLength(6)
   password?: string;
+
+  @IsString()
+  @IsOptional()
+  firstName?: string;
+
+  @IsString()
+  @IsOptional()
+  lastName?: string;
+
+  @IsString()
+  @IsOptional()
+  avatar?: string;
 }
